39bfac2c69c7db2676521c567d0cdfa5bf8b4f13
NoahsarkTaiwan <ben109092ben@gmail.com>
2024-06-19 03:43:08 +0800

    update

--- js/toggleLyrics.js
+++ js/toggleLyrics.js
@@ -1,14 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const tabs = document.querySelectorAll('.tab-button');
-    const contents = document.querySelectorAll('.tab-content');
-
-    tabs.forEach(tab => {
-        tab.addEventListener('click', function() {
-            tabs.forEach(t => t.classList.remove('active'));
-            contents.forEach(c => c.classList.remove('active'));
-
-            tab.classList.add('active');
-            document.getElementById(tab.getAttribute('data-tab')).classList.add('active');
-        });
-    });
-});
+const { createApp } = Vue;
+createApp({
+    data() {
+        return {
+            selectedLanguage: 'original',
+        };
+    }
+}).mount('#app');
